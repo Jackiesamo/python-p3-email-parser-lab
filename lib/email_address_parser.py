357bc22f5d78df4243d74908db9d785003ce17a0
jacklyne <jacklyne.owuor@student.moringaschool>
2025-12-05 11:20:41 +0300

    added upgraded file

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,13 @@
-# your code goes here!
+
+# email_address_parser.py
+import re
+
+class EmailAddressParser:
+    def __init__(self, text):
+        self.text = text
+
+    def parse(self):
+        # Find all emails in the text
+        emails = re.findall(r"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}", self.text)
+        # Return sorted list alphabetically
+        return sorted(emails)
